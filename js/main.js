05ffceeddb7431964866ee29d798ff554c62de7f
Javier Martin Gil <javiermartingil83@gmail.com>
2019-09-12 11:47:45 +0200

    Busqueda y filtro

--- js/main.js
+++ js/main.js
@@ -2,8 +2,45 @@ const baseUrl = 'https://rickandmortyapi.com/api/character/';
 
 const container = document.querySelector('.container');
 const selector = document.getElementById('status');
+const buscar = document.getElementById('buscar');
 let resultado = '';
+let resultado_pet = '';
+let personajes = '';
 
+
+buscar.addEventListener('input', (event) => {
+    fetch(`${baseUrl}`)
+        .then(response => response.json())
+        .then(characters => personajes = characters.results)
+        .then(personajes => {
+            console.log(personajes);
+            let personajesHtml = "";
+            console.log(event.target.value)
+            for (personaje of personajes) {
+                if (personaje.name.toLowerCase().includes(event.target.value.toLowerCase()))
+                    personajesHtml += createCharacterHtml(personaje)
+            }
+            container.innerHTML = personajesHtml;
+        })
+})
+
+
+selector.addEventListener('change', (event) => {
+    console.log(event.target.value);
+    fetch(`${baseUrl}?status=${event.target.value}`)
+
+        .then(response => response.json())
+        .then(characters => personajes = characters.results)
+        .then(personajes => {
+            console.log(personajes);
+            let personajesHtml = "";
+            for (personaje of personajes) {
+                personajesHtml += createCharacterHtml(personaje);
+            }
+            container.innerHTML = personajesHtml;
+        });
+
+})
 
 // Función para recuperar a todos los personajes
 function getAll() {
@@ -11,43 +48,23 @@ function getAll() {
         .then(response => response.json())
         .then(characters => {
             let personajes = characters.results;
-            /* let resultado = ''; */
-            // console.log(personajes)
+
             for (personaje of personajes) {
-                resultado += `<div class="personaje">
-                             <img src="${personaje.image}" alt="">
-                               <h2>${personaje.name}</h2>
-                             <h4>Status: ${personaje.status}</h4>
-                            <a href="" class="btn">Mas info</a>
-                             </div>`
+                resultado += createCharacterHtml(personaje);
             }
             container.innerHTML = resultado;
-            //return resultado;
+
         });
 }
 
-// Función para recuperar a los personajes según su estado
-/* function getState(pEstado) {
-    fetch(`${baseUrl}?status=${pEstado}`)
-        .then(response => response.json())
-        .then(characters => resultado = characters);
-    console.log(resultado)
-} */
-
-const seleccionarSTatus = selector.addEventListener('change', (pEstado) => {
-    fetch(`${baseUrl}?status=${selector.value}`)
-    console.log(selector.value)
-        .then(response => response.json())
-        .then(characters => resultado_pet = characters);
-    resultado = '';
-    resultado_pet += `<div class="personaje">
-                    <img src="${personaje.image}" alt="">
-                    <h2>${personaje.name}</h2>
-                    <h4>Status: ${personaje.status}</h4>
-                    <a href="" class="btn">Mas info</a>
-                </div>`
-    container.innerHTML = resultado;
-})
+function createCharacterHtml(character) {
+    return `<div class="personaje">
+                    <img src="${character.image}" alt="">
+                    <h2>${character.name}</h2>
+                    <h4>Status: ${character.status}</h4>
+                    <a href="singlecharacter/${character.id}" class="btn">Mas info</a>
+                </div>`;
+}
 
 // Función para recuperar a los personajes por id
 function getSingle(pId) {
